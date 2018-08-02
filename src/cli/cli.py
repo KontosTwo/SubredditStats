8d2581023675dda326581f5b050f4f7b49f42500
Vincent Li <vincentli@vincentli-mbp.spice.spiceworks.com>
2018-08-02 16:16:52 -0500

    app runs the bare minimum

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
 
-import click
+import argparse
 import requests
+import sys
+import string
 
-def main():
-    pass
+parser = argparse.ArgumentParser(prog='PROG')
+subparsers = parser.add_subparsers(help='sub-command help')
 
-@click.command()
-@click.option('--subreddit', prompt="Subreddit")
+mcwbs = subparsers.add_parser('mcwbs', help='Most common words by subreddit')
+mcwbs.add_argument('-s','--subreddit')
+
 def most_common_words_by_subreddit(subreddit):
     message = requests.get("http://localhost:5000/most-common-words-by-subreddit/" + subreddit)
-    print(message.content)
+
 
 if __name__ == "__main__":
-    most_common_words_by_subreddit()
+    args = parser.parse_args(sys.argv[1:])
+    if(sys.argv[1] == "mcwbs"):
+        most_common_words_by_subreddit(args.subreddit)
+
